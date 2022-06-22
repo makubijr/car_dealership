94d5da831fa08bdcf1b2bf7252690d2877f313d0
cipher <fynhacker@gmail.com>
2022-06-22 19:40:36 +0300

    first commit

---
+++ listings/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here
+
+class Listing(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    brand = models.CharField(max_length=100)
+    milage = models.IntegerField()
+    price = models.IntegerField()
+    #image 
+
+    def __str__(self):
+        return self.name 
